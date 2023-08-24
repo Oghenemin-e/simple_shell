2965b5fe07bb227877d8fbbdac727da60267588c
Oghenemin-e <lawalogheneminebabatunde@gmail.com>
2023-08-24 21:02:11 +0100

    Delete shell.h

--- shell.h
+++
@@ -1,8 +0,0 @@
-#ifndef SHELL_H
-#define SHELL_H
-
-int execute_command(const char **args);
-char *parse_input(const char *input);
-int set_environment_variable(const char *name, const char *value);
-
-#endif /* SHELL_H */
